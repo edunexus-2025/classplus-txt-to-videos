81bf3f4fe869b64d9ac150f3fb6bb9441cbdd2ee
edunexus-2025 <edunexustestplatform@gmail.com>
2025-08-07 22:21:06 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,8 @@
-api_id = 25218674
-api_hash = "87c231f0e8704795b8239d06965b4351"
-bot_token = "6326230595:AAHGRQLrdDp-RiXyfNe9veBxwabOju-ldSY"
-auth_users = [6805344720]
-sudo_user = 6805344720
+api_id = 4321233
+api_hash = "8732e6fd352098398ca71f9866635fa7"
+bot_token = "7983040092:AAH-i1R3U9ZNCtwj_Gk6DGG9neGq-w4LTeY"
+auth_users = [5666274513]
+sudo_user = 5666274513
 log_channel = -1002038285084
 txt_channel = -1002038285084
+
